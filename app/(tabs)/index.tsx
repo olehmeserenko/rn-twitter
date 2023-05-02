17f07c1a6630f9d3a5b2787a8515de8ff5c0f2f7
Oleh Meserenko <meserenko.oleg@gmail.com>
2023-05-02 10:06:49 -0400

    [CORE] Add new tweet page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,7 @@
+import { Entypo } from '@expo/vector-icons'
 import { FlatList, StyleSheet, View } from 'react-native'
+
+import { Link } from 'expo-router'
 
 import { Tweet } from '@/components/Tweet'
 import { tweets } from '~/assets/data/tweets'
@@ -10,6 +13,14 @@ export default function TabOneScreen() {
         data={tweets}
         renderItem={({ item }) => <Tweet tweet={item} />}
       />
+      <Link href={'/new-tweet'} asChild>
+        <Entypo
+          name={'plus'}
+          size={24}
+          color={'white'}
+          style={styles.floatingButton}
+        />
+      </Link>
     </View>
   )
 }
@@ -18,5 +29,22 @@ const styles = StyleSheet.create({
   page: {
     backgroundColor: 'white',
     flex: 1,
+  },
+  floatingButton: {
+    backgroundColor: '#1C9BF0',
+    padding: 15,
+    borderRadius: 25,
+    position: 'absolute',
+    bottom: 15,
+    right: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    overflow: 'hidden',
   },
 })
